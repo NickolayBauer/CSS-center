88301892037b6a161790b93639cdf37936292209
Nickolas <35115545+NickolayBauer@users.noreply.github.com>
2018-11-22 23:54:01 +0300

    Update center.js

--- center.js
+++ center.js
@@ -1,17 +1,31 @@
 //2018.22.10 
 //Даннный файл поможет справится с выравниванием
 //
+//Для работы необходим Jquery
 //
+//<script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 //Выравнивание по вертикали
 //
 //
+//
 
-var mass_outer = document.getElementsByClassName("outer-vert");
-var mass_inner = document.getElementsByClassName("inner-vert")
+var mass_outer_vert = document.getElementsByClassName("outer-vert");
+var mass_inner_vert = document.getElementsByClassName("inner-vert");
 
-for (i = 0; i<= mass_outer.length-1; i++)
+for (var i = 0; i <= mass_outer_vert.length - 1; i++)
 {
-    $(mass_inner[i]).css("margin-top",parseInt((mass_outer[i].offsetHeight- mass_inner[i].offsetHeight)/2);+"px");
+    $(mass_inner_vert[i]).css("margin-top",parseInt((mass_outer_vert[i].offsetHeight- mass_inner_vert[i].offsetHeight)/2)+"px");
 }
 
+//Выравнивание по горизонтали
 //
+//
+//
+
+var mass_outer_hori = document.getElementsByClassName("outer-hori");
+var mass_inner_hori = document.getElementsByClassName("inner-hori");
+
+for (var i = 0; i <= mass_outer_hori.length - 1; i++)
+{
+    $(mass_inner_hori[i]).css("margin-left",parseInt((mass_outer_hori[i].offsetWidth- mass_inner_hori[i].offsetWidth)/2)+"px");
+}
